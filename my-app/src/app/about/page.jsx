7e77561c9d4b704f174caf43c55eccd192258b2b
Ahmad-Haymour <92795077+Ahmad-Haymour@users.noreply.github.com>
2023-12-14 02:36:33 +0100

    initial commit

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/Button/Button'
 
 const About = () => {
   return (
@@ -14,8 +16,38 @@ const About = () => {
       </div>
       <div className={styles.textContainer}>
         <h1 className={styles.imgTitle}>Who Are We?</h1>
-        <h2 className={styles.imgDec}></h2>
+        <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            <br />
+            <br />
+            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+            officiis voluptatum quo ea eveniet?
+          </p>
       </div>
+      <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
+            Creative Illustrations
+            <br />
+            <br /> - Dynamic Websites
+            <br />
+            <br /> - Fast and Handy
+            <br />
+            <br /> - Mobile Apps
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
     </div>
   )
 }
